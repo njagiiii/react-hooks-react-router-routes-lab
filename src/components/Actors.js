e8c3756cb2beeeee36e795184c7600a7ff326955
njagiiii <grace.makena@student.moringaschool.com>
2023-07-27 11:54:29 -0400

    completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,21 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  let idCounter = 0;
+  const actorList = actors.map((actor) =>{
+    const moviee = (actor.movies).map((mov) => <li key={++idCounter}>{mov}</li>)
+
+    return(
+      <div key={++idCounter}>
+        <h1>{actor.name}</h1>
+        <ul>{moviee}</ul>
+      </div>
+    )
+  })
+  return <div>
+    <h1>Actors Page</h1>
+    <ul>{actorList}</ul>
+  </div>;
 }
 
 export default Actors;
